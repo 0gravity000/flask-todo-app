b5f00bbc1eb2b60bad015240286348df21a2ada4
0gravity000 <0gravit000@gmail.com>
2021-09-22 18:20:26 +0900

    todo.dbの準備

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
 
 @app.route('/')
 def index():
@@ -8,3 +17,11 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# 実行
+# python app.py
+
+# todo.dbの作成
+# >>> python
+# >>> from app import db
+# >>> db.create_all()
